fb13d32d620851244446c6caacb8907097e1e93b
Rod <rod@kafer.com.au>
2026-02-19 16:32:35 +0000

    Fix eslint config - use compat.extends

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,16 @@
-// Empty ESLint config - Next.js handles this internally
-export default [];
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
